03f93f9e9dbc2cc6cbb30db39213b1ad81efb6d1
Your Name <you@example.com>
2018-05-19 00:16:13 +0200

    Added a config file where hardcoded values can be kept

---
+++ src/GemCollectorConfig.java
@@ -0,0 +1,18 @@
+
+public class GemCollectorConfig {
+	// timer
+	public static final int TIMER_UPDATE_DELAY = 10;
+	
+	// board
+	public static final int TILE_SIZE = 40;
+
+	// gem slot
+	public static final int GEM_SLOT_DIAMETER = TILE_SIZE;
+	
+	// top bar
+	public static final int TOP_BAR_HEIGHT = TILE_SIZE + 10;
+	
+	// frame
+	public static final int BOARD_WIDTH = TILE_SIZE * 20;
+	public static final int BOARD_HEIGHT = TOP_BAR_HEIGHT + 10 * TILE_SIZE;
+}

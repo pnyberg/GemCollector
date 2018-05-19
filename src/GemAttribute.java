4a88dd1516707fb7897aaa63a08e0a9a483dbba0
Your Name <you@example.com>
2018-05-19 12:09:43 +0200

    Added attribute-class for gems

---
+++ src/GemAttribute.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public enum GemAttribute {
+	PURPLE_STONE("./images/purple_stone.png", new Color(138,43,226)),
+	RED_STONE("./images/red_stone.png", Color.red),
+	BLUE_STONE("./images/blue_stone.png", Color.blue),
+	GREEN_STONE("./images/green_stone.png", Color.green),
+	ORANGE_STONE("./images/orange_stone.png", Color.orange);
+	
+	private Image image;
+	private Color color;
+
+	GemAttribute(String imagePath, Color color) {
+		this.image = Toolkit.getDefaultToolkit().getImage(imagePath);
+		this.color = color;
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+}
